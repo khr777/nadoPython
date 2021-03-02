fc1dff85d845e5cee6fef04beba3ca480d9b6af4
khr777 <kim5638yw@gmail.com>
2021-03-02 19:14:53 +0900

    Python 변수 사용 방법 기초
    정수, boolean 자료형을 str로 형변환해서 사용.
    + 연산자 대신 , 사용 가능하지만 참고해야할 2가지 사항.
    첫번째 ,를 사용하면 str 형변환을 하지 않아도 된다.
    두번째 ,를 사용하면 뛰어쓰기가 1칸 자동으로 입력된다.

--- practiceVar.py
+++ practiceVar.py
@@ -1,1 +1,20 @@
 # 변수 
+# 애완동물을 소개해 주세요~ 
+animal = "강아지"
+name = "연탄이"
+age = 4
+hobby = "산책"
+is_adult = age >= 3
+
+print(animal)
+print(name)
+print(age)
+
+print("-----------------------")
+
+print("우리집 " + animal +"의 이름은 " + name + "예요")
+# print(name + "는 " + str(age) + "살이며, " + hobby + "을 아주 좋아해요")
+# + 대신 , 사용 가능하지만 ,를 사용하는 경우에는 띄어쓰기가 한칸씩 들어가게 된다.
+# 그리고 문자열이 아닌 타입을 str로 형변환 해주지 않아도 사용 가능하다.
+print(name, "는 ", age, "살이며, ", hobby, "을 아주 좋아해요")
+print(name + "는 어른일까요? " + str(is_adult))
