ee45c46ec2f096f854e26763aea62cf28d2c6394
khr777 <kim5638yw@gmail.com>
2021-03-02 20:02:13 +0900

    Python 간단한 수식 연습
    +=, -=, *=, /=, %= 등 간단한 수식 연습.

---
+++ practice2.py
@@ -0,0 +1,18 @@
+# 간단한 수식
+
+print(2 + 3 * 4) # 14
+print((2 + 1) * 3) # 9
+number = 3 + 1 * 3 # 6
+print(number) # 6
+number = number + 2
+print(number) # 8
+number += 2 # 10
+print(number)
+number *= 2 # 20
+print(number)
+number /= 2 # 10
+print(number)
+number -= 5 # 5
+print(number)
+number %= 2 # 1
+print(number)
