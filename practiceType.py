85ab8362f6f72d7d1b159b045833ca498c3242a3
khr777 <kim5638yw@gmail.com>
2021-03-02 18:52:40 +0900

    Python  자료형 기초 연습 내용
    문자열, 숫자, 참/거짓(boolean), not

---
+++ practiceType.py
@@ -0,0 +1,23 @@
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(5+3)
+print(3*10)
+print(3*(3+1))
+
+
+
+print("풍선")
+print('나비')
+print("ㅋ"*10)
+
+# 참 / 거짓
+print(5 > 10)
+print(10 > 5 )
+print(True)
+print(False)
+print(not True)
+print(not False)
+print("boolean practice---------")
+print(not ( 5 > 10))
