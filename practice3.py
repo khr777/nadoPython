6dcba976de4a36288846e169fe65ab6133e4563b
khr777 <kim5638yw@gmail.com>
2021-03-02 20:15:36 +0900

    숫자 처리 함수 연습
    절대값, 제곱, 최대값, 최소값,  반올림.
    math 라이브러리(floor, ceil, sqrt(제곱근) 함수.

---
+++ practice3.py
@@ -0,0 +1,33 @@
+# 숫자 처리 함수
+
+# 절대값 : abs() 함수 
+print(abs(-5)) # 5
+
+# 제곱(2가지 방법)
+print(pow(4, 2)) # 16
+print(4 ** 2) # 16
+
+# 최대값
+print(max(5, 10)) # 10
+
+# 최소값
+print(min(10, 2)) # 2
+
+# 반올림
+print(round(3.14)) # 3
+print(round(2.99)) # 3
+
+# math 라이브러리 사용법
+# 'math 라이브러리 안에 있는 모든것(*)을 사용하겠다.' 라는 의미.
+from math import * 
+# floor : 내림 (소수점 아래 부분을 모두 내림)
+print(floor(4.88)) # 4
+
+# ceil : 올림 / ceiling
+print(ceil(3.11)) # 4
+
+# 제곱근 구하기
+print(sqrt(9)) # 3
+
+
+
