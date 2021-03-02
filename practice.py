bb203725cdae7b28e1e7d9f5c831dacb245aff94
khr777 <kim5638yw@gmail.com>
2021-03-02 19:51:03 +0900

    Python 연산자 연습
    연산자 기초 연습.
    or, and, 대소 관계 연습.

---
+++ practice.py
@@ -0,0 +1,31 @@
+# 연산자
+print(1+1)
+print(3-1)
+print(3*2)
+print(6/3)
+
+
+print(2**3) # 2^3 = 8
+print(5%3) # 나머지 2
+print(10%3) # 나머지 1
+
+print(5//3) # 몫 1
+print(10//3) # 몫 3
+
+print(10 > 3)
+print(4 >= 7)
+print(10 < 3)
+print(5 <= 5)
+
+print(3 == 3)
+print(1 != 3)
+
+
+print((3 > 0) and (3 < 5)) # True
+print((3 > 0) & (3 < 5)) # True
+
+print((2 > 0) or (2 < 1)) # True
+print((2 > 0) | (2 < 1)) # True
+
+print(5 > 4 > 3) # True
+print(5 > 4 > 7) # False
