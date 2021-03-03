f09ee76cbeb54eae6380863405a36d542ffca6a6
khr777 <kim5638yw@gmail.com>
2021-03-03 14:25:03 +0900

    탈출문자/개행문자 사용 연습
    줄바꿈, 따옴표 입력, 역슬러쉬, 백스페이스 등등 사용 연습

---
+++ str.py
@@ -0,0 +1,23 @@
+# 탈출 문자 
+# \n : 줄바꿈
+print("백문이 불여일견\n백견이 불여일타")
+
+# \" \' : 문장 내에서 따옴표
+# 저는 "나도코딩"입니다.
+print('저는 "나도코딩"입니다.') # '(작은따옴표)로 감싸고 문자열 안에 "(큰따옴표) 입력
+print("저는 \"나도코딩\"입니다.")
+print("저는 \'나도코딩\'입니다.")
+
+# \\ : 문장 내에서 \(1개)
+print("c:\\Users\\kim56\\OneDrive\\바탕 화면\\PythonWorkspace\\str.py")
+
+# \r : 커서를 맨 앞으로 이동
+# \r 뒤의 문자를 맨 앞으로 이동시켜서 index에 맞춰 치환한다.
+print("Red Apple\rDDD BANANA")
+print("Test Str\r보라돌이 테스트")
+
+# \b : 백스페이스 (한 글자 삭제)
+print("Redd\bApple")
+
+# \t : 탭 (4칸/8칸씩 이동할 때)
+print("Red\tApple")
