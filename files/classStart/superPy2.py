3780ec53d568d6e1352584631e7255ab20597b7e
khr777 <kim5638yw@gmail.com>
2021-03-07 19:23:45 +0900

    파일 다시 저장
    파일 다시 저장.

--- files/classStart/superPy2.py
+++ files/classStart/superPy2.py
@@ -12,6 +12,9 @@ class FlyableUnit(Unit, Flyable): # Flyable을 먼저 쓰면 Flyable 생성자�
         Unit.__init__(self)
         Flyable.__init__(self)        
 
+# 결론 : 다중 상속을 받을 경우에는 super() 사용을 권장하지 않는다.
+# 명시적으로 init을 통하여 부모 클래스를 호출하도록 한다.        
+
 # 드랍쉽 : 유닛을 공중으로 태우고만 다니는 로봇, 공격할 수 없음.
 dropShip = FlyableUnit() # 출력 : Unit 생성자 / Flyable 생성자는 초기화 되지 않았다.
 
