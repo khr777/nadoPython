f9fafa7702ac2dae5983ca068e27bc1e54d2c9b0
khr777 <kim5638yw@gmail.com>
2021-03-07 17:58:05 +0900

    클래스 상속
    클래스를 상속하므로써 중복되는 코드를 줄일 수 있음.
    자식 클래스에서는 객체를 생성할 때 부모 클래스를 호출하여 필요한 변수를
    저장할 수 있음.

--- files/classStart/extendsPy2.py
+++ files/classStart/extendsPy2.py
@@ -1,24 +1,23 @@
-# 메소드
+# 다중 상속
 
 # 일반 유닛
 class Unit:
-    def __init__(self, name, hp, damage):
+    def __init__(self, name, hp):
         self.name = name
         self.hp = hp
-        self.damage = damage
-        print("{0} 유닛이 생성 되었습니다.".format(self.name))
-        print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
 
+# 상속받을 때 '클래스명(상속받을 클래스명)'
 # 공격 유닛
-class AttackUnit:
+class AttackUnit(Unit):
     def __init__(self, name, hp, damage):
-        self.name = name
-        self.hp = hp
+        Unit.__init__(self, name, hp) # 부모에게 값을 넘겨주어 초기화하는 작업.
         self.damage = damage            
+        
     def attack(self, location):
         print("{0} : {1} 방향으로 적군을 공격 합니다. [공격력 : {2}]".format(
             self.name, location, self.damage
         ))
+        
     def damaged(self, damage):
         print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
         self.hp -= damage
@@ -27,6 +26,7 @@ class AttackUnit:
             print("{0} : 파괴되었습니다.".format(self.name))
             
 
+# 메딕 : 의무병 (공격력 : 0 / 없음)
 
 # 파이어뱃 : 공격 유닛, 화염방사기.
 firebat1 = AttackUnit("파이어뱃", 50, 16)            
@@ -35,3 +35,4 @@ firebat1.attack("5시")
 # 공격 2번 받는다고 가정
 firebat1.damaged(25)
 firebat1.damaged(25)
+
