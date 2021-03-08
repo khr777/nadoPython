f999250b7c480c45ec7389abf9d197f15ef9d564
khr777 <kim5638yw@gmail.com>
2021-03-08 23:50:00 +0900

    모듈이 잘 작동하는지 테스트 하는 방법(모듈 직접 실행)
    모듈이 잘 작동하는지 테스트 하는 방법(모듈 직접 실행).
    if문을 통해서 각 모듈마다 잘 작동하는지 테스트를 한 후,
    모듈을 사용한다.

--- travel/thailand.py
+++ travel/thailand.py
@@ -1,3 +1,14 @@
 class ThailandPackage:
     def detail(self):
         print("[태국 패키지 3박 5일] 방콕, 파타야 여행 (야시장 투어) 50만원")
+
+
+# 모듈 직접 실행        
+# 모듈이 잘 동작하는지 사전에 테스트를 해보아야 한다.         
+if __name__ == "__main__":
+    print("Thailand 모듈을 직접 실행")
+    print("이 문장은 모듈을 직접 실행할 때만 실행돼요")
+    trip_to = ThailandPackage()
+    trip_to.detail()
+else:
+    print("Thailand 외부에서 모듈 호출")
